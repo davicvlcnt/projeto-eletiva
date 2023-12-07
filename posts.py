6aa3da6fdffd90bb817dc64f271e22ee56458f62
davicvlcnt <146394839+davicvlcnt@users.noreply.github.com>
2023-12-06 22:25:51 -0300

    projeto eletiva

---
+++ posts.py
@@ -0,0 +1,27 @@
+posts = [
+    {
+        'titulo': 'Veja tdo sobre os especial de 60 anos da série Dr who ',
+        'conteudo': 'Uma nova foto do especial de 60 anos de Doctor Who marcou o retorno de David Tennant à TARDIS como o 14º Doutor (via Empire).',
+        'data': '24.10.2023',
+        'url': 'https://www.omelete.com.br/doctor-who/doctor-who-david-tennant-tardis'
+    },
+    {
+        'titulo': 'Trailer do filme sobre o Bob Marley ',
+        'conteudo': 'A Paramount Pictures divulgou um novo trailer de Bob Marley: One Love. A cinebiografia é estrelada por Kingsley Ben-Adir e conta a história de superação por trás da música revolucionária de Bob Marley',
+        'data': '05.12.2023',
+        'url': 'https://www.omelete.com.br/filmes/one-love-novo-trailer-bob-marley'
+    },
+    {
+        'titulo': 'Tudo que sabemos sobre a nova série de Aliens',
+        'conteudo': 'A saga promete ser independente dos dois filmes dirigidos por Ridley Scott em 2011 e 2017',
+        'data': '24.10.2023',
+        'url': 'https://www.terra.com.br/diversao/entre-telas/alien-data-de-lancamento-sinopse-elenco-e-tudo-o-que-sabemos-sobre-a-serie-inspirada-nos-filmes,9f99c8f83874445637bafa668ca77181ojhors3a.html'
+    },
+    {
+        'titulo': 'Planeta dos macacos: o reinado',
+        'conteudo': 'Data de lançamento, elenco e história já foi divulgada.',
+        'data': '24.10.2023',
+        'url': 'https://br.ign.com/planeta-dos-macacos-o-reinado/112051/feature/planeta-dos-macacos-o-reinado-data-de-lancamento-elenco-historia-e-tudo-sobre-o-proximo-filme-da-fra'
+
+    },
+]
